44c5a4484d591caea872fa8643b6136643bc0196
Kristian Kime <kristian.kime@gmail.com>
2012-10-30 15:11:20 -0400

    removed unnecisairy constraint in contact manager

--- src/example/java/contactmanager/model/ContactManager.java
+++ src/example/java/contactmanager/model/ContactManager.java
@@ -6,27 +6,22 @@ import com.artc.javafx.collections.BeanObservableList;
 
 public class ContactManager {
 	private final ObservableList<Contact> contacts;
-	
+
 	public ContactManager(Contact... contacts) {
 		this.contacts = BeanObservableList.create(contacts, Contact.FIRST_NAME, Contact.LAST_NAME);
-		if(this.contacts.size() < 1){
-			throw new IllegalArgumentException("Must have at least one contact");
-		}
 	}
-	
+
 	public Contact createNewContact() {
 		Contact contact = new Contact();
 		contacts.add(contact);
 		return contact;
 	}
-	
+
 	public ObservableList<Contact> getContacts() {
 		return contacts;
 	}
-	
+
 	public void removeContact(Contact contact) {
-		if (contacts.size() > 1) {
-			contacts.remove(contact);
-		}
+		contacts.remove(contact);
 	}
 }
